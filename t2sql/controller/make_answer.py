0d017fe34a4e0d274c7d23b84b0b0ff5f1598b5d
Kirill Kirikov <kk@datrics.ai>
2025-03-11 22:37:52 +0000

    Initial commit

---
+++ t2sql/controller/make_answer.py
@@ -0,0 +1,25 @@
+from t2sql.utils import logger
+import pandas as pd
+from t2sql.base import BaseText2SQLAgent
+
+
+async def make_answer(query: str, agent: BaseText2SQLAgent) -> str:
+    logger.info(f"[{agent._descriptor_folder}] Extracting sql for query: {query}...")
+
+    sql, _ = await agent.make_sql(query)
+
+    logger.info(f"[{agent._descriptor_folder}] Generated sql: {sql}")
+
+    return sql
+
+
+async def run_sql(sql: str, agent: BaseText2SQLAgent) -> tuple[str, pd.DataFrame]:
+    sql, _, data = await agent.execute_sql(sql)
+
+    logger.info(f"[{agent._descriptor_folder}] Returning data for sql: {len(data)}...")
+    return sql, data
+
+
+async def run_all(query: str, agent: BaseText2SQLAgent) -> tuple[str, pd.DataFrame]:
+    sql = await make_answer(query, agent)
+    return await run_sql(sql, agent)
